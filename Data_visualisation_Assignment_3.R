f7eacf659a3d05cfdf476c29632bd2aeb36a86e8
Tommon93 <thomasle093@gmail.com>
2025-06-07 16:03:51 +1000

    Updated to remove demo file

---
+++ Data_visualisation_Assignment_3.R
@@ -0,0 +1,9 @@
+# This script will create a shiny app that visualizes ABS CPI data
+# across Australia.
+
+# Load necessary libraries
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+# Load the ABS CPI data
